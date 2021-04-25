94be0623cb77879b120394728d46a69cd387ea58
Noel Hsiao <huihsinhsiao12@gmail.com>
2021-04-25 16:43:36 +0800

    新增googlemap_api&marker

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,2 +1,5 @@
 module LocationsHelper
+  def google_map(center)
+    "https://maps.googleapis.com/maps/api/staticmap?markers=size:small%7Ccolor:orange%7Clabel:C%7Ccenter=#{center}&size=500x500&zoom=17&key=#{Rails.application.credentials.google_api_key}"
+  end
 end
